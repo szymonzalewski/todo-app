f202deae358fba30424469acd3384c6a98e74938
szymonzalewski <szymon.marek.zalewski@gmail.com>
2025-02-27 22:07:02 +0100

    Initial commit - First version of To-Do App

--- script.js
+++ script.js
@@ -94,7 +94,7 @@ function taskAdded() {
   function getCurrentDateTime() {
     let now = new Date();
     let day = String(now.getDate()).padStart(2, "0");
-    let month = String(now.getMonth() + 1).padStart(2, "0"); // Miesiące zaczynają się od 0
+    let month = String(now.getMonth() + 1).padStart(2, "0");
     let year = now.getFullYear();
     let hours = String(now.getHours()).padStart(2, "0");
     let minutes = String(now.getMinutes()).padStart(2, "0");
